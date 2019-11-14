6369974629fa7a6d58a267deb8e855a2f95b29ce
JeffRisberg <jrisberg@aol.com>
2019-11-14 13:48:48 -0800

    updated formatting

--- src/main/java/samples/DynamoDBSample.java
+++ src/main/java/samples/DynamoDBSample.java
@@ -6,8 +6,6 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.ListTablesRequest;
 import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 import java.io.IOException;
 import java.util.List;
@@ -22,61 +20,61 @@ import java.util.List;
  */
 public class DynamoDBSample {
 
-    public static void main(String[] args) throws IOException {
-        /*
-         * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users)
-         * and save the following lines after replacing the underlined values with your own.
-         *
-         * [default]
-         * aws_access_key_id = YOUR_ACCESS_KEY_ID
-         * aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
-         */
+  public static void main(String[] args) throws IOException {
+    /*
+     * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users)
+     * and save the following lines after replacing the underlined values with your own.
+     *
+     * [default]
+     * aws_access_key_id = YOUR_ACCESS_KEY_ID
+     * aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
+     */
 
-        final String region = "us-west-2";
-        final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(new ProfileCredentialsProvider("default"))
-                .withRegion(region)
-                .build();
+    final String region = "us-west-2";
+    final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.standard()
+      .withCredentials(new ProfileCredentialsProvider("default"))
+      .withRegion(region)
+      .build();
 
-        System.out.println("Your DynamoDB tables:\n");
+    System.out.println("Your DynamoDB tables:\n");
 
-        ListTablesRequest request;
+    ListTablesRequest request;
 
-        boolean more_tables = true;
-        String last_name = null;
+    boolean more_tables = true;
+    String last_name = null;
 
-        while (more_tables) {
-            try {
-                if (last_name == null) {
-                    request = new ListTablesRequest().withLimit(10);
-                } else {
-                    request = new ListTablesRequest()
-                            .withLimit(10)
-                            .withExclusiveStartTableName(last_name);
-                }
-
-                ListTablesResult table_list = ddb.listTables(request);
-                List<String> table_names = table_list.getTableNames();
-
-                if (table_names.size() > 0) {
-                    for (String cur_name : table_names) {
-                        System.out.format("* %s\n", cur_name);
-                    }
-                } else {
-                    System.out.println("No tables found!");
-                    System.exit(0);
-                }
-
-                last_name = table_list.getLastEvaluatedTableName();
-                if (last_name == null) {
-                    more_tables = false;
-                }
-
-            } catch (AmazonServiceException e) {
-                System.err.println(e.getErrorMessage());
-                System.exit(1);
-            }
+    while (more_tables) {
+      try {
+        if (last_name == null) {
+          request = new ListTablesRequest().withLimit(10);
+        } else {
+          request = new ListTablesRequest()
+            .withLimit(10)
+            .withExclusiveStartTableName(last_name);
         }
-        System.out.println("\nDone!");
+
+        ListTablesResult table_list = ddb.listTables(request);
+        List<String> table_names = table_list.getTableNames();
+
+        if (table_names.size() > 0) {
+          for (String cur_name : table_names) {
+            System.out.format("* %s\n", cur_name);
+          }
+        } else {
+          System.out.println("No tables found!");
+          System.exit(0);
+        }
+
+        last_name = table_list.getLastEvaluatedTableName();
+        if (last_name == null) {
+          more_tables = false;
+        }
+
+      } catch (AmazonServiceException e) {
+        System.err.println(e.getErrorMessage());
+        System.exit(1);
+      }
     }
+    System.out.println("\nDone!");
+  }
 }
--- src/main/java/samples/EC2Sample.java
+++ src/main/java/samples/EC2Sample.java
@@ -20,68 +20,68 @@ import java.io.IOException;
  */
 public class EC2Sample {
 
-    public static void main(String[] args) throws IOException {
-        /*
-         * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users)
-         * and save the following lines after replacing the underlined values with your own.
-         *
-         * [default]
-         * aws_access_key_id = YOUR_ACCESS_KEY_ID
-         * aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
-         */
+  public static void main(String[] args) throws IOException {
+    /*
+     * Create your credentials file at ~/.aws/credentials (C:\Users\USER_NAME\.aws\credentials for Windows users)
+     * and save the following lines after replacing the underlined values with your own.
+     *
+     * [default]
+     * aws_access_key_id = YOUR_ACCESS_KEY_ID
+     * aws_secret_access_key = YOUR_SECRET_ACCESS_KEY
+     */
 
-        final String region = "us-west-2";
-        final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard()
-                .withCredentials(new ProfileCredentialsProvider("default"))
-                .withRegion(region)
-                .build();
+    final String region = "us-west-2";
+    final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard()
+      .withCredentials(new ProfileCredentialsProvider("default"))
+      .withRegion(region)
+      .build();
 
-        System.out.println("===========================================");
-        System.out.println("Getting Started with Amazon EC2");
-        System.out.println("===========================================\n");
+    System.out.println("===========================================");
+    System.out.println("Getting Started with Amazon EC2");
+    System.out.println("===========================================\n");
 
-        boolean done = false;
+    boolean done = false;
 
-        try {
-            DescribeInstancesRequest request = new DescribeInstancesRequest();
-            while (!done) {
-                DescribeInstancesResult response = ec2.describeInstances(request);
+    try {
+      DescribeInstancesRequest request = new DescribeInstancesRequest();
+      while (!done) {
+        DescribeInstancesResult response = ec2.describeInstances(request);
 
-                for (Reservation reservation : response.getReservations()) {
-                    for (Instance instance : reservation.getInstances()) {
-                        System.out.printf(
-                                "Found instance with id %s, " +
-                                        "AMI %s, " +
-                                        "type %s, " +
-                                        "state %s " +
-                                        "and monitoring state %s",
-                                instance.getInstanceId(),
-                                instance.getImageId(),
-                                instance.getInstanceType(),
-                                instance.getState().getName(),
-                                instance.getMonitoring().getState());
-                    }
-                }
-
-                request.setNextToken(response.getNextToken());
-
-                if (response.getNextToken() == null) {
-                    done = true;
-                }
-            }
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, which means your request made it "
-                    + "to Amazon EC2, but was rejected with an error response for some reason.");
-            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId());
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with EC2, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message: " + ace.getMessage());
+        for (Reservation reservation : response.getReservations()) {
+          for (Instance instance : reservation.getInstances()) {
+            System.out.printf(
+              "Found instance with id %s, " +
+                "AMI %s, " +
+                "type %s, " +
+                "state %s " +
+                "and monitoring state %s",
+              instance.getInstanceId(),
+              instance.getImageId(),
+              instance.getInstanceType(),
+              instance.getState().getName(),
+              instance.getMonitoring().getState());
+          }
         }
+
+        request.setNextToken(response.getNextToken());
+
+        if (response.getNextToken() == null) {
+          done = true;
+        }
+      }
+    } catch (AmazonServiceException ase) {
+      System.out.println("Caught an AmazonServiceException, which means your request made it "
+        + "to Amazon EC2, but was rejected with an error response for some reason.");
+      System.out.println("Error Message:    " + ase.getMessage());
+      System.out.println("HTTP Status Code: " + ase.getStatusCode());
+      System.out.println("AWS Error Code:   " + ase.getErrorCode());
+      System.out.println("Error Type:       " + ase.getErrorType());
+      System.out.println("Request ID:       " + ase.getRequestId());
+    } catch (AmazonClientException ace) {
+      System.out.println("Caught an AmazonClientException, which means the client encountered "
+        + "a serious internal problem while trying to communicate with EC2, "
+        + "such as not being able to access the network.");
+      System.out.println("Error Message: " + ace.getMessage());
     }
+  }
 }
